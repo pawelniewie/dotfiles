9917e4ec4b52ce057986a29a5c5638444172e9a1
Pawel Niewiadomski <11110000b@gmail.com>
2017-11-30 07:09:44 +0100

    added sublw

--- shell/brew.zsh
+++ shell/brew.zsh
@@ -2,7 +2,6 @@ if [ -d "/home/linuxbrew" ]; then
 	export PATH="/home/linuxbrew/.linuxbrew/bin:$PATH"
 	export MANPATH="/home/linuxbrew/.linuxbrew/share/man:$MANPATH"
 	export INFOPATH="/home/linuxbrew/.linuxbrew/share/info:$INFOPATH"
-	export PATH="/home/linuxbrew/.linuxbrew/bin:$PATH"
 fi
 
 export HOMEBREW_CASK_OPTS="--appdir=/Applications"
