2e1fdd41f427a8368039a942f9036ce9067f46a7
Pawel Niewiadomski <11110000b@gmail.com>
2018-04-11 12:32:43 +0200

    removed obsolete java opts, sbt_opts no longer needed

--- shell/maven.zsh
+++ shell/maven.zsh
@@ -1,5 +1,6 @@
-export MAVEN_OPTS="-Xmx768m -XX:MaxPermSize=256m"
-if [ -f /usr/libexec/java_home ] ; then
+export MAVEN_OPTS="-Xmx768m"
+
+if [ -x /usr/libexec/java_home ] ; then
 	export MAVEN_OPTS="$MAVEN_OPTS -Djavax.net.ssl.trustStore=`/usr/libexec/java_home`/jre/lib/security/cacerts"
 	export ATLAS_OPTS="-Djavax.net.ssl.trustStore=`/usr/libexec/java_home`/jre/lib/security/cacerts"
 fi
